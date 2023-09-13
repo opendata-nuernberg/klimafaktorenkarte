e47582cbffc348b73b863d2c414bd263aaa4949d
Jonas Siedentop <Jonas@Sieden.top>
2023-09-13 12:20:20 +0200

    converter tool

--- tools/sensor_converter/bin/sensor_converter.dart
+++ tools/sensor_converter/bin/sensor_converter.dart
@@ -1,5 +1,18 @@
-import 'package:sensor_converter/sensor_converter.dart' as sensor_converter;
+import 'package:args/command_runner.dart';
+import 'package:sensor_converter/sensor_converter.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${sensor_converter.calculate()}!');
+Future<void> main(List<String> arguments) async {
+  try {
+    final runner = CommandRunner(
+        "convert", "cli tool to convert sensor data from sensor.community")
+      ..addCommand(ConvertCommand());
+
+    await runner.run(arguments);
+  } catch (ex) {
+    if (ex is UsageException) {
+      print(ex.usage);
+    } else {
+      print(ex);
+    }
+  }
 }
--- tools/sensor_converter/lib/sensor_converter.dart
+++ tools/sensor_converter/lib/sensor_converter.dart
@@ -1,3 +1,23 @@
-int calculate() {
-  return 6 * 7;
+import 'package:args/command_runner.dart';
+
+class ConvertCommand extends Command {
+  // The [name] and [description] properties must be defined by every
+  // subclass.
+  @override
+  final name = "convert";
+  @override
+  final description = "converts sensor data from sensor.community";
+
+  ConvertCommand() {
+    // we can add command specific arguments here.
+    // [argParser] is automatically created by the parent class.
+    // argParser.addFlag('all', abbr: 'a');
+    //addSubcommand(SubCommand());
+  }
+
+  // [run] may also return a Future.
+  @override
+  void run() {
+    print(argResults?['all']);
+  }
 }
