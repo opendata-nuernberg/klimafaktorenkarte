cf8f86b5c80d93677acfd81a8a50a9bbc0c23b32
Jonas Siedentop <Jonas@Sieden.top>
2023-09-13 11:39:59 +0200

    sensor converter

---
+++ tools/sensor_converter/bin/sensor_converter.dart
@@ -0,0 +1,5 @@
+import 'package:sensor_converter/sensor_converter.dart' as sensor_converter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sensor_converter.calculate()}!');
+}
---
+++ tools/sensor_converter/test/sensor_converter_test.dart
@@ -0,0 +1,8 @@
+import 'package:sensor_converter/sensor_converter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
